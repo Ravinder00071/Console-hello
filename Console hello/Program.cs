d4deab32e4a54095519f2989477db7b54463a677
Ravinder Sandhu <RS076@atcltd.atc.org.nz>
2020-05-11 12:58:11 +1200

    added comments

---
+++ Console hello/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Console_hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+            Console.ReadLine();
+        }
+    }
+}
